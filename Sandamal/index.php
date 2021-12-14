8f62e41882c5c30a3ba4870d35479d6d422e7315
mltshope <96107532+mltshope@users.noreply.github.com>
2021-12-14 14:19:01 +0530

    Add files via upload

---
+++ Sandamal/index.php
@@ -0,0 +1,20 @@
+<<html>
+<head>
+	<title>Sandamal's Shope</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="navbar">
+		<div class="logo">
+			<img src="images/logo.png">
+
+
+		</div>
+
+
+
+	</div>
+
+
+</body>
+</html>
